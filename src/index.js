8d45600ecfcb7ad35f2898ca23c83c520dfe5131
Jose <josedhg29@gmail.com>
2023-05-22 12:42:57 -0400

    fabric clear button

--- src/index.js
+++ src/index.js
@@ -10,4 +10,12 @@ function run() {
       //     left: 100,
       //     top: 100,
       //     fill: 'red',
-      /
+      //     width: 20,
+      //     height: 20
+      //   });
+      //   canvas.add(rect);
+      var clearEl = $('clear-canvas');
+      clearEl.onclick = function() { canvas.clear() };
+    }
+    
+run();
