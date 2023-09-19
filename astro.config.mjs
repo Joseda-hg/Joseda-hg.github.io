beabec763e7d39836104dcb34880b408d5a6f8fe
Jose <josedhg29@gmail.com>
2023-09-19 13:39:54 -0300

    Prototipo Ideas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import vercelServerless from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: vercelServerless()
