53314bdd9ba3ae3fdb1f5d239394d48a0f0b89e8
Jose <josedhg29@gmail.com>
2023-09-23 17:10:05 -0300

    Try with the books too

---
+++ src/pages/[bookName].json.ts
@@ -0,0 +1,23 @@
+export const prerender = true;
+import json from "../books.json";
+export async function getStaticPaths() {
+  // @ts-ignore
+  return json.map((book) => {
+    return {
+      params: { bookName: book.Text },
+      props: { book },
+    };
+  });
+}
+// @ts-ignore
+export async function get({ params, request }) {
+  const id = params.bookName;
+  return {
+    body: JSON.stringify({
+      //@ts-ignore
+      nombre: `${json[id - 1].Title}`,
+      //@ts-ignore
+      url: `https://www.gutenberg.org/ebooks/${json[id - 1].Text}.epub3.images`,
+    }),
+  };
+}
---
+++ src/pages/[id].epub.js
@@ -0,0 +1,12 @@
+export const prerender = false;
+export const config = {
+  runtime: 'serverless',
+};
+export async function get({ params, request }) {
+    const id = params.id;
+    const response = await fetch(`https://www.gutenberg.org/ebooks/${id}.epub3.images`);
+    const buffer = Buffer.from(await response.arrayBuffer());
+    return new Response(buffer, {
+      headers: { "Content-Type": "application/epub+zip"},
+    });
+  }
